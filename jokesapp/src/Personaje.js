8a8d0f7822b3575fb9bd778b57235b69402fb2a9
boriscm <69550620+boriscm@users.noreply.github.com>
2022-04-21 21:04:54 -0500

    personaje

--- jokesapp/src/Personaje.js
+++ jokesapp/src/Personaje.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 
-function personaje() {
+function Personaje() {
   let [characters, setCharacters] = useState([]);
 
   useEffect(() => {
@@ -12,11 +12,9 @@ function personaje() {
             setCharacters(JSON.parse(localStorage.getItem("characters")))
         }
     } else {
-        // CombiParam (ts + private key + public key)
-        // eslint-disable-next-line no-useless-concat
+
         const urlAPI = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=5940fff28db1e6a4206df6e802b2d6da&hash=5e093566eddf081a45296163589247c9`
         fetch(urlAPI).then((res) => res.json()).then((data) => {
-            console.log("Respose", data.data.results);
             setCharacters(data.data.results);
             console.log(JSON.stringify(data.data.results))
             localStorage.setItem("characters", JSON.stringify(data.data.results));
@@ -35,4 +33,4 @@ function personaje() {
   );
 }
 
-export default personaje;
+export default Personaje;
